0a0af72a3bb2c81b898405c828621e526cd779f5
mdarwish2020 <112318952+mdarwish2020@users.noreply.github.com>
2022-09-13 21:51:39 -0400

    Add files via upload

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by user226639 on 9/13/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
